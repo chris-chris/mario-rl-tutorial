6c3a1c47ba137e73d55b4cf1197843bacb99a844
Chris <sjhshy@gmail.com>
2017-09-23 23:48:43 +0900

    apply acktr(a3c) algorithm

--- train.py
+++ train.py
@@ -1,8 +1,39 @@
-import gym
+#!/usr/bin/env python
+import os, logging, gym
+from baselines import logger
+from baselines.common import set_global_seeds
+from baselines import bench
+from acktr_disc import learn
+from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
+from baselines.common.atari_wrappers import wrap_deepmind
+from policies import CnnPolicy
+#from gym.envs.ppaquette_gym_super_mario.wrappers.action_space import ToDiscrete
 import ppaquette_gym_super_mario
-env = gym.make('ppaquette/SuperMarioBros-1-1-v0')
-observation = env.reset()
-for _ in range(1000):
-  env.render()
-  action = env.action_space.sample() # your agent here (this takes random actions)
-  observation, reward, done, info = env.step(action)
+
+def train(env_id, num_timesteps, seed, num_cpu):
+  num_timesteps //= 4
+
+  def make_env(rank):
+    def _thunk():
+      env = gym.make(env_id)
+      env.seed(seed + rank)
+      if logger.get_dir():
+        env = bench.Monitor(env, os.path.join(logger.get_dir(), "{}.monitor.json".format(rank)))
+      gym.logger.setLevel(logging.WARN)
+
+      return env
+    return _thunk
+
+  set_global_seeds(seed)
+  env = SubprocVecEnv([make_env(i) for i in range(num_cpu)])
+
+  policy_fn = CnnPolicy
+  learn(policy_fn, env, seed, total_timesteps=num_timesteps, nprocs=num_cpu)
+  env.close()
+
+def main():
+  train('ppaquette/SuperMarioBros-1-2-v0', num_timesteps=int(40e6), seed=0, num_cpu=4)
+
+
+if __name__ == '__main__':
+  main()
