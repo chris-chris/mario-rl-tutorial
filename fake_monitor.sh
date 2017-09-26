23c885ac27c864bc5c8ab024a5c6111aacd7c0d8
Chris <sjhshy@gmail.com>
2017-09-27 00:09:20 +0900

    add fake_monitor shell script

---
+++ fake_monitor.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# for CentOS
+sudo yum install -y
+
+# for Ubuntu
+sudo apt-get install xvfb -y
+
+# Crate fake monitor
+Xvfb :99 -ac -screen 0 1280x1024x24 > /dev/null &
+
+# Set environment
+echo "export DISPLAY=:99" >> ~/.profile
+source ~/.profile
